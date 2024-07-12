61d77c3fe01fc2b19ed382b937d801a3ae05403e
Dev <97352849+Vicdiamond@users.noreply.github.com>
2024-07-12 23:39:15 +0100

    fixed more styling

--- src/components/LogoAndTitle.js
+++ src/components/LogoAndTitle.js
@@ -16,10 +16,10 @@ function LogoAndTitle ({ title, icon, isDarkmode }) {
   // console.log(isDarkmode)
   return (
     <>
-      <div className={`${getBackgroundClass(title)} p-2 rounded-lg`}>
+      <div className={`${getBackgroundClass(title)} p-2 rounded-lg z-10`}>
         <img src={icon} alt={`${title} icon`} className='h-6' />
       </div>
-      <p className={`${isDarkmode ? 'text-white' : 'text-[#313E51]'}`}>
+      <p className={`${isDarkmode ? 'text-white' : 'text-[#313E51]'} z-10`}>
         {title}
       </p>
     </>
